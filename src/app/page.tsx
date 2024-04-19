5173eddb9ad061ffd4e71327601fe609ba0fa44f
Asmit tyagi <ashmittyagigms@gmail.com>
2024-04-19 21:21:08 +0530

    Adddomains: ['images.unsplash.com'] Add Instructor Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import { TestimonialCards } from "@/components/TestimonialCards";
 import UpcomimgWebinars from "@/components/UpcomimgWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs/>
       <TestimonialCards/>
       <UpcomimgWebinars/>
+      <Instructors/>
     </main>
   );
 }
